8a8c9fbd763ac6d9f8f91ae617db8d140598180b
reggiegupta <g.rahul.gupta@gmail.com>
2016-06-28 12:00:00 +0530

    test
    test

---
+++ MyTestAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MyTestAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
